e786d71e28bb6712c3a09e0110ae8eb6e91f9125
njy622 <njy622@naver.com>
2023-11-22 10:12:59 +0900

    11.22

---
+++ 02.남지영/onnanana/src/main/java/com/example/onnanana/OnnananaApplication.java
@@ -0,0 +1,13 @@
+package com.example.onnanana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnananaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnnananaApplication.class, args);
+	}
+
+}
---
+++ 02.남지영/onnanana/src/test/java/com/example/onnanana/OnnananaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onnanana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnananaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
