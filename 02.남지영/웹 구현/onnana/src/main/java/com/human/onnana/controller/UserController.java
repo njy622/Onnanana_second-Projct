9b999bb4b82ae47fe1566c5526fd18664182a8e7
njy622 <njy622@naver.com>
2023-12-05 00:05:38 +0900

    탄소배출량 계산기 구현

---
+++ 02.남지영/웹 구현/onnana/src/main/java/com/human/onnana/controller/UserController.java
@@ -0,0 +1,186 @@
+package com.human.onnana.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.human.onnana.entity.User;
+import com.human.onnana.service.ScheduleService;
+import com.human.onnana.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired private UserService userService;
+	@Autowired private ScheduleService schedService;
+
+
+	@GetMapping("/update/{uid}")
+	@ResponseBody	
+	//HTTP 요청의 분문 body 부분이 그대로 전달(xml이나 json기반의s메시지를 사용하는 요청의 경우)
+	public String updateForm(@PathVariable String uid) {
+		User user = userService.getUser(uid);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("uid", user.getUid());
+		jsonObject.put("uname", user.getUname());
+		jsonObject.put("email", user.getEmail());
+		return jsonObject.toJSONString();
+	}
+	
+	@PostMapping("/update")
+	public String updateProc(String pwd, String pwd2, String uname, 
+			 String email, HttpSession session, Model model) {
+		String uid = (String) session.getAttribute("sessUid");
+		User user = userService.getUser(uid); 
+		// 패스워드 미입력시 어떻게 표현되는지 확인하기 위해 출력해봄(출력값: pwd=,pwd2=)
+		// System.out.println("pwd="+ pwd + ",pwd2="+pwd2); 
+		
+		// 올바른 패스워드 규칙을 맞췄을 경우, 패스워드 변경
+		if (pwd.length() >=  4 && pwd.equals(pwd2)) {
+			String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+			user.setPwd(hashedPwd);
+		
+		// 아무런 데이터를 입력하지 않은 경우, 패스워드 변경하지 않음
+		} else if (pwd.equals("") && pwd2.equals("")) {
+			;				//아무일도 하지 않는다.
+		//그외에 경고 메세지를 보내준다.
+		} else {
+			model.addAttribute("msg", "패스워드를 다시 입력하고 수정하세요.");
+			model.addAttribute("url", "/onnana/user/list/" + session.getAttribute("currentUserPage"));
+			return "common/alertMsg";
+		}
+		user.setUname(uname);
+		user.setEmail(email);
+		userService.updateUser(user);
+		
+		
+		return "redirect:/user/list/" + session.getAttribute("currentUserPage");
+	}
+	
+	@GetMapping("/delete/{uid}")
+	   public String delete(@PathVariable String uid, HttpSession session) {
+		userService.deleteUser(uid);
+		return"redirect:/user/list/"+ session.getAttribute("currentUserPage");
+   }
+
+	
+	
+	
+	
+	@GetMapping("/list/{page}")
+	public String list(@PathVariable int page, HttpSession session, Model model) {
+		List<User> list = userService.getUserList(page);
+		model.addAttribute("userList", list);
+		
+		int totalUsers = userService.getUserCount();
+		int totalPages = (int) Math.ceil((double)totalUsers / userService.RECORDS_PER_PAGE);
+		List<String> pageList = new ArrayList<>();
+		for (int i=1; i<=totalPages; i++)
+			pageList.add(String.valueOf(i));
+		model.addAttribute("pageList", pageList);
+		session.setAttribute("currentUserPage", page);
+		model.addAttribute("menu", "user"); // 유저아이콘 불들어옴
+		
+		return "user/list";
+	}
+	
+	
+	@GetMapping("/login")
+	public String loginForm() {
+		return "user/login";
+	}
+	
+	@PostMapping("/login")
+	public String loginProc(String uid, String pwd, HttpSession session, Model model) {
+		int result = userService.login(uid, pwd);
+		if (result == userService.CORRECT_LOGIN) {
+			session.setAttribute("sessUid", uid);
+			User user = userService.getUser(uid);
+			session.setAttribute("sessUname", user.getUname());
+			session.setAttribute("sessEmail", user.getEmail());
+			
+			// 게시판 글 전체 카운트
+			session.setAttribute("sessAllId", schedService.getCount());
+			// 게시판 글 유저 카운트
+			session.setAttribute("sessId", schedService.getUserCount(uid));
+						
+			
+			// 환영 메세지
+			// 로그인 입력 잘못해도, home으로 바로 이동
+			model.addAttribute("msg", user.getUname() + "님 환영합니다.");
+			model.addAttribute("url", "/onnana/home");
+		} else if (result == userService.WRONG_PASSWORD) {
+			model.addAttribute("msg", "패스워드 입력이 잘못되었습니다.");
+			model.addAttribute("url", "/onnana/home");
+		} else {		// UID_NOT_EXIST
+			model.addAttribute("msg", "ID 입력이 잘못되었습니다.");
+			model.addAttribute("url", "/onnana/home");
+		}
+		return "common/alertMsg";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/home";		// 로그아웃시, 홈으로 가도록 설정
+	}
+	
+	@GetMapping("/register")
+	public String registerForm() {
+		return "user/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerProc(String uid, String pwd, String pwd2, 
+								String uname, String email, Model model) {
+		if (userService.getUser(uid) != null) {
+			model.addAttribute("msg", "사용자 ID가 중복되었습니다.");
+			model.addAttribute("url", "/onnana/user/register");
+		}
+		if (pwd.equals(pwd2) && pwd.length() >= 4) {	// pwd와 pwd2가 같고, 길이가 4이상이면
+			String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+			User user = new User(uid, hashedPwd, uname, email);
+			userService.insertUser(user);
+			model.addAttribute("msg", "등록을 마쳤습니다. 로그인 하세요.");
+			model.addAttribute("url", "/onnana/home");
+		} else {
+			model.addAttribute("msg", "패스워드 입력이 잘못되었습니다.");
+			model.addAttribute("url", "/onnana/user/register");
+		}
+		return "common/alertMsg";
+	}
+	
+	
+	
+	
+	
+	@GetMapping("/analysis")
+	public String analysisForm() {
+		
+		return "user/analysis";
+	}
+	
+
+	@GetMapping("/weather")
+	public String weatherForm() {
+		
+		return "user/weather";
+	}
+	@GetMapping("/dust")
+	public String dustForm() {
+		
+		return "user/dust";
+	}
+}
---
+++ 02.남지영/웹 구현/onnana/src/main/java/com/human/onnana/service/ScheduleServiceOracleImpl.java
@@ -0,0 +1,90 @@
+package com.human.onnana.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.human.onnana.db.AnniversaryDaoOracle;
+import com.human.onnana.db.ScheduleDaoOracle;
+import com.human.onnana.entity.Anniversary;
+import com.human.onnana.entity.SchDay;
+import com.human.onnana.entity.Schedule;
+
+@Service
+public class ScheduleServiceOracleImpl implements ScheduleService {
+	
+	@Autowired private ScheduleDaoOracle schedDao;
+	@Autowired private AnniversaryDaoOracle annivDao;
+
+	@Override
+	public List<Schedule> getDaySchedList(String uid, String sdate) {
+		List<Schedule> list = schedDao.getSchedList(uid, sdate, sdate);
+		return list;
+	}
+
+	@Override
+	public List<Schedule> getMonthSchedList(String uid, String month, int lastDay) {
+		String startDay = month + "01";
+		String endDay = month + lastDay;
+		List<Schedule> list = schedDao.getSchedList(uid, startDay, endDay);
+		return list;
+	}
+
+	@Override
+	public List<Schedule> getCalendarSchedList(String uid, String startDate, String endDate) {
+		List<Schedule> list = schedDao.getSchedList(uid, startDate, endDate);
+		return list;
+	}
+
+	@Override
+	public SchDay generateSchDay(String uid, String sdate, int date, int isOtherMonth) {
+		List<Anniversary> annivList = annivDao.getAnnivList(uid, sdate, sdate);
+		List<Schedule> schedList = schedDao.getSchedList(uid, sdate, sdate);
+		int day = Integer.parseInt(sdate.substring(6));
+		int isHoliday = 0;
+		List<String> aList = new ArrayList<>();
+		for (Anniversary anniv: annivList) {
+			aList.add(anniv.getAname());
+			if (isHoliday == 0)
+				isHoliday = anniv.getIsHoliday();
+		}
+		SchDay schDay = new SchDay(day, date, isHoliday, isOtherMonth, sdate, aList, schedList);
+		return schDay;
+	}
+
+	@Override
+	public void insert(Schedule schedule) {
+		schedDao.insert(schedule);
+	}
+
+	@Override
+	public Schedule getSchedule(int sid) {
+		Schedule sched = schedDao.getSchedule(sid);
+		return sched;
+	}
+
+	@Override
+	public void update(Schedule schedule) {
+		schedDao.update(schedule);
+	}
+
+	@Override
+	public void delete(int sid) {
+		schedDao.delete(sid);
+	}
+	
+	@Override
+	public int getCount() {
+		int allCount = schedDao.count();
+		return allCount;
+	}
+
+	@Override
+	public int getUserCount(String uid) {
+		int userCount = schedDao.userCount(uid);
+		return userCount;
+	}
+
+}
