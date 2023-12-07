e6561d3f25866805dbc6f1636fc3440c4f882479
njy622 <njy622@naver.com>
2023-12-07 17:38:25 +0900

    탄소계산기 - 글삭제시 실시간 카운트 구현작업

---
+++ 02.남지영/웹 구현/onnana/onnana/src/test/java/com/human/onnana/OnnanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.onnana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
