2bae33d00c48a8517786edb12bd87bb6ccf8d054
Maru230403 <136799101+Maru230403@users.noreply.github.com>
2023-12-11 12:38:02 +0900

    23.12.11

--- 01.심민준/08.Prototype/app.py
+++ 01.심민준/08.Prototype/app.py
@@ -7,106 +7,107 @@ import csv
 from draw_map import  BORDER_LINES, drawKorea, drawKoreaMinus
 
 app = Flask(__name__)
+CORS(app)
 
-# 서버 시작 전에 실행될 함수
-@app.before_first_request
-def before_first_request():
+# # 서버 시작 전에 실행될 함수
+# @app.before_first_request
+# def before_first_request():
 
-    # 기본 URL 및 API 키 설정
-    base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
-    with open('keys/에어코리아api2.txt') as file:
-        service_key = file.read()  # 발급받은 에어코리아 API 키 입력
+#     # 기본 URL 및 API 키 설정
+#     base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
+#     with open('static/keys/에어코리아api2.txt') as file:
+#         service_key = file.read()  # 발급받은 에어코리아 API 키 입력
 
-    # 예제 데이터프레임 생성
-    main = pd.read_csv('data/카토그램_최종.csv', encoding='utf-8')
+#     # 예제 데이터프레임 생성
+#     main = pd.read_csv('static/data/카토그램_최종.csv', encoding='utf-8')
 
-    # 결과를 저장할 리스트
-    results_list = []
+#     # 결과를 저장할 리스트
+#     results_list = []
 
-    # 요청을 나누어 보내기
-    chunk_size = 50
-    max_retries = 3
+#     # 요청을 나누어 보내기
+#     chunk_size = 50
+#     max_retries = 3
 
-    for i in range(0, len(main), chunk_size):
-        chunk = main.iloc[i:i+chunk_size]
+#     for i in range(0, len(main), chunk_size):
+#         chunk = main.iloc[i:i+chunk_size]
         
-        # 각 측정소에 대한 데이터 수집
-        for index, row in chunk.iterrows():
-            retry_count = 0
+#         # 각 측정소에 대한 데이터 수집
+#         for index, row in chunk.iterrows():
+#             retry_count = 0
 
-            while retry_count < max_retries:
-                try:
-                    params = {
-                        'serviceKey': service_key,
-                        'returnType': 'JSON',
-                        'numOfRows': 24,
-                        'pageNo': 1,
-                        'stationName': row['측정소명'],
-                        'dataTerm': 'DAILY',
-                        'ver': "1.4"
-                    }
+#             while retry_count < max_retries:
+#                 try:
+#                     params = {
+#                         'serviceKey': service_key,
+#                         'returnType': 'JSON',
+#                         'numOfRows': 24,
+#                         'pageNo': 1,
+#                         'stationName': row['측정소명'],
+#                         'dataTerm': 'DAILY',
+#                         'ver': "1.4"
+#                     }
 
-                    res = requests.get(url=base_url, params=params)
+#                     res = requests.get(url=base_url, params=params)
 
-                    # 응답 데이터 정리
-                    response = res.json()['response']['body']
+#                     # 응답 데이터 정리
+#                     response = res.json()['response']['body']
                     
-                    # 데이터가 존재하면 처리
-                    if 'items' in response and response['items']:
-                        data = response['items']
+#                     # 데이터가 존재하면 처리
+#                     if 'items' in response and response['items']:
+#                         data = response['items']
                         
-                        # 데이터를 날짜와 시간에 대한 기준으로 정렬
-                        sorted_data = sorted(data, key=lambda x: x['dataTime'], reverse=True)
+#                         # 데이터를 날짜와 시간에 대한 기준으로 정렬
+#                         sorted_data = sorted(data, key=lambda x: x['dataTime'], reverse=True)
 
-                        # 최신 데이터 선택
-                        latest_data = None
+#                         # 최신 데이터 선택
+#                         latest_data = None
 
-                        for data in sorted_data:
-                            pm10_value = data.get('pm10Value')
-                            if pm10_value and pm10_value != '-':
-                                latest_data = data
-                                break
+#                         for data in sorted_data:
+#                             pm10_value = data.get('pm10Value')
+#                             if pm10_value and pm10_value != '-':
+#                                 latest_data = data
+#                                 break
 
-                        # 최신 데이터가 없으면 첫 번째 데이터 선택
-                        if not latest_data and sorted_data:
-                            latest_data = sorted_data[0]
+#                         # 최신 데이터가 없으면 첫 번째 데이터 선택
+#                         if not latest_data and sorted_data:
+#                             latest_data = sorted_data[0]
 
-                        result = {
-                            '날짜': latest_data['dataTime'],
-                            '이름': latest_data['stationName'],
-                            '측정망 정보': latest_data['mangName'],
-                            '아황산가스 농도': latest_data['so2Value'],
-                            '일산화탄소 농도': latest_data['coValue'],
-                            '오존 농도': latest_data['o3Value'],
-                            '이산화질소 농도': latest_data['no2Value'],
-                            '미세먼지(PM10) 농도': latest_data.get('pm10Value', 0),
-                            '초미세먼지(PM2.5) 농도': latest_data.get('pm25Value', 0)
-                        }
+#                         result = {
+#                             '날짜': latest_data['dataTime'],
+#                             '이름': latest_data['stationName'],
+#                             '측정망 정보': latest_data['mangName'],
+#                             '아황산가스 농도': latest_data['so2Value'],
+#                             '일산화탄소 농도': latest_data['coValue'],
+#                             '오존 농도': latest_data['o3Value'],
+#                             '이산화질소 농도': latest_data['no2Value'],
+#                             '미세먼지(PM10) 농도': latest_data.get('pm10Value', 0),
+#                             '초미세먼지(PM2.5) 농도': latest_data.get('pm25Value', 0)
+#                         }
 
-                        results_list.append(result)
-                        break  # 성공한 경우 while 루프 종료
-                    else:
-                        results_list.append({})
-                        break  # 성공했지만 데이터가 비어있는 경우 while 루프 종료
+#                         results_list.append(result)
+#                         break  # 성공한 경우 while 루프 종료
+#                     else:
+#                         results_list.append({})
+#                         break  # 성공했지만 데이터가 비어있는 경우 while 루프 종료
 
-                except requests.exceptions.RequestException as e:
-                    print(f"Request failed: {e}")
-                    retry_count += 1
-                    print(f"Retrying... (Attempt {retry_count}/{max_retries})")
+#                 except requests.exceptions.RequestException as e:
+#                     print(f"Request failed: {e}")
+#                     retry_count += 1
+#                     print(f"Retrying... (Attempt {retry_count}/{max_retries})")
 
-                    if retry_count == max_retries:
-                        print(f"Max retries reached. Skipping request for {row['측정소명']}")
-                        break  # 최대 재시도 횟수에 도달하면 while 루프 종료
+#                     if retry_count == max_retries:
+#                         print(f"Max retries reached. Skipping request for {row['측정소명']}")
+#                         break  # 최대 재시도 횟수에 도달하면 while 루프 종료
 
-    # 결과 리스트의 길이를 250개로 맞추기
-    results_list.extend([{}] * (len(main) - len(results_list)))
+#     # 결과 리스트의 길이를 250개로 맞추기
+#     results_list.extend([{}] * (len(main) - len(results_list)))
 
-    # '미세먼지' 데이터를 숫자로 변환하고, 빈 데이터는 0으로 변경
-    main['미세먼지'] = pd.to_numeric([entry.get('미세먼지(PM10) 농도', 0) for entry in results_list], errors='coerce')
-    main['미세먼지'] = main['미세먼지'].fillna(0)
+#     # '미세먼지' 데이터를 숫자로 변환하고, 빈 데이터는 0으로 변경
+#     main['미세먼지'] = pd.to_numeric([entry.get('미세먼지(PM10) 농도', 0) for entry in results_list], errors='coerce')
+#     main['미세먼지'] = main['미세먼지'].fillna(0)
 
-    # '미세먼지' 처리 이후에 그래프 그리기
-    drawKorea('미세먼지', main, 'Blues', save_filename='../onnana/src/main/resources/static/img/카토그램.png')
+#     # '미세먼지' 처리 이후에 그래프 그리기
+#     drawKorea('미세먼지', main, 'Blues', save_filename='../onnana/src/main/resources/static/img/카토그램.png')
 
 def get_weather(nx, ny):
     base_url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
@@ -191,7 +192,7 @@ def get_weather(nx, ny):
     return results
 
 def get_air_quality(station_name):
-    with open('static/keys/에어코리아api.txt') as file:
+    with open('static/keys/에어코리아api2.txt') as file:
         air_service_key = file.read()
     air_base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
 
