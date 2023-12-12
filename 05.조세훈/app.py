5037008cd417d56bbc2a82b528b8837db7931e34
Maru230403 <136799101+Maru230403@users.noreply.github.com>
2023-12-12 17:29:59 +0900

    20231212 생활지수 구현

--- 05.조세훈/app.py
+++ 05.조세훈/app.py
@@ -2,12 +2,71 @@
 from flask import Flask, render_template
 import pandas as pd
 import json
+import requests, time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from datetime import datetime, timedelta
+from selenium.webdriver.common.by import By
+
 
 # Flask 웹 애플리케이션을 생성합니다.
 app = Flask(__name__)
 
 # 웹 브라우저에서 '/test' 경로로 접속했을 때 실행되는 함수를 정의합니다.
 @app.route('/test')
+def crawl_and_save_to_csv():
+    data1 = []
+    data2 = []
+    url = 'https://www.kr-weathernews.com/mv3/html/lifeindex.html?region='
+    header = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
+    res = requests.get(url, headers=header) 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    trs = soup.select('#slidePage1 > #pointList > li')
+    try:
+        for tr in trs:
+            img = tr.select_one('img')['src']
+            item = tr.select_one('h1').get_text().strip().replace('.', '')
+            index = tr.select_one('h2').get_text().strip()
+            ment = tr.select_one('p').get_text().strip()
+
+            url = 'https://www.kr-weathernews.com/mv3/html/lifeindex.html?region='
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+
+            data1.append({ '이미지': img, '생활지수': item, '지수': index, '안내멘트': ment})
+
+
+        # 계절별 지수를 찾아서 클릭
+        driver.find_element(By.XPATH, '//*[@id="2"]').click()
+        time.sleep(1)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        trs2 = soup.select('#slidePage2 > #pointList > li')
+        for tr2 in trs2:
+            img = tr2.select_one('img')['src']
+            item = tr2.select_one('h1').get_text().strip().replace('.', '')
+            index = tr2.select_one('h2').get_text().strip()
+            ment = tr2.select_one('p').get_text().strip()
+
+
+            url = 'https://www.kr-weathernews.com/mv3/html/lifeindex.html?region='
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+
+            data2.append({ '이미지': img, '생활지수': item, '지수': index, '안내멘트': ment})
+    finally:
+        driver.quit()
+        data = data1 + data2
+        df = pd.DataFrame(data)
+        df.to_csv('data/kweather.csv', index=False)
+        # 응답을 생성하여 반환
+    return display_data()
+
 def display_data():
     # CSV 파일에서 데이터프레임을 읽어옵니다.
     file_path = 'data/kweather.csv'
